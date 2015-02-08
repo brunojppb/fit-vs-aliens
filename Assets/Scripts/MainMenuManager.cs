8da2177d902ecd0afb5c26f014d641779e4ba192
Bruno Paulino <brunojppb@gmail.com>
2015-02-08 16:20:35 -0500

    Adds main menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuManager : MonoBehaviour {
+
+	public void LoadGameplay(){
+		Application.LoadLevel("Gameplay");
+	}
+
+	public void ShowCreditsScreen(){
+
+	}
+}
